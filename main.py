98f532a27cec8f1030260ac9768078360f746326
kmans <kmansuri@gmail.com>
2015-06-05 17:25:02 -0400

    chat randomizer work in progress
    mobile now works extremely well on newer devices, chat randomizer
    enabled in flask, experimenting with moving to redis backend from sqlite

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, url_for, redirect, flash
 
+
+#for random chatroom hash
+import binascii
+import os
+
+
 #For db
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -31,9 +37,20 @@ class Comment(db.Model):
     comment_id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, nullable=False)
-    def __init__(self, text, timestamp):
+    room = db.Column(db.Text, nullable=False)
+
+    def __init__(self, text, timestamp, room):
         self.text = text
         self.timestamp = timestamp
+        self.room = room
+
+
+
+
+
+#generate a very random hex value
+def createRoom():
+    return binascii.hexlify(os.urandom(3))
 
 
 #Form for the textfield
@@ -43,20 +60,21 @@ class CommentForm(Form):
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/<int:chatsession>', methods=['GET', 'POST'])
-def index(chatsession=0):
+@app.route('/<room>', methods=['GET', 'POST'])
+def index(room='000000'):
     form = CommentForm()
     if form.validate_on_submit():
         comment = Comment(
             form.text.data,
-            datetime.datetime.now()
+            datetime.datetime.now(),
+            room
         )
         db.session.add(comment)
         db.session.commit()
-        flash("comment added on " + comment.timestamp.strftime('%Y-%m-%d %H:%M:%S'))
-        return redirect(url_for('index', chatsession=chatsession))
+        flash("comment added on " + comment.timestamp.strftime('%Y-%m-%d %H:%M:%S') + " for room " + room)
+        return redirect(url_for('index', room=room))
     comments = Comment.query.order_by(db.desc(Comment.timestamp))
-    return render_template('index.html', comments=comments, form=form)
+    return render_template('index.html', comments=comments, form=form, room=room, new=createRoom())
 
 
 def static(path):
@@ -72,7 +90,7 @@ def context_processor():
 
 
 
+if __name__ == "__main__":
 
-
-#app.run(debug=True)
-app.run()
+    #app.run(debug=True)
+    app.run()
