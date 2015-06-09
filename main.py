cdbbc77b7f752d7e6011e69d3b5ecd4b0c69cb83
kmans <kmansuri@gmail.com>
2015-06-09 13:36:33 -0400

    sign-up working!
    now we must get sign-in working on the mainpage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, flash
-
+from flask import Flask, render_template, url_for, redirect, flash, request, session
 
 #for random chatroom hash
 import binascii
@@ -9,12 +8,13 @@ import os
 #For db
 from flask.ext.sqlalchemy import SQLAlchemy
 
-#For user login / session management
-from flask.ext.login import LoginManager
+#For user login / session management / password hashing
+from werkzeug import generate_password_hash, check_password_hash
+
 
 #WTForms / Flask-WTForms
 from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField, BooleanField, validators
+from wtforms import TextField, PasswordField, SubmitField, validators
 
 import datetime
 import urlparse
@@ -54,8 +54,17 @@ class User(db.Model):
     __bind_key__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(80), nullable=False)
-    remember = db.Column(db.Boolean)
+    pwdhash = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email.lower()
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.pwdhash = generate_password_hash(password)
+   
+    def check_password(self, password):
+        return check_password_hash(self.pwdhash, password)
 
 
 #create all the databases
@@ -76,14 +85,53 @@ class CommentForm(Form):
     text = TextField('Comment', [validators.Required()])
 
 
-#Form for logging in (basic auth)
 class UserForm(Form):
-    email = TextField('Email Address', [validators.Required(), validators.Email()])
-    password = PasswordField('Password', [validators.Required()])
-    remember = BooleanField('Remember Me')
+    email = TextField('Email Address', [validators.Required("Please enter your email address."), validators.Email("Please enter your email address")])
+    password = PasswordField('Password', [validators.Required("Please enter a valid password")])
+    submit = SubmitField("Create account")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+ 
+    def validate(self):
+        if not Form.validate(self):
+          return False
+     
+        user = User.query.filter_by(email = self.email.data.lower()).first()
+        if user:
+            self.email.errors.append("That email is already taken")
+            return False
+        else:
+            return True
 
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  form = UserForm()
+   
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+
+        form.password.data    
+        newuser = User(form.email.data, form.password.data)
+        db.session.add(newuser)
+        db.session.commit()
+
+        session['email'] = newuser.email
+
+        return redirect(url_for('index'))
+
+   
+  elif request.method == 'GET':
+    return render_template('signup.html', form=form)
+
+
+
+
+'''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
@@ -100,12 +148,18 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', form=form)
-
+'''
 
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/<room>', methods=['GET', 'POST'])
 def index(room='000000'):
+
+    #we first check if user has a session cookie and is logged in
+    if 'email' in session:
+        user = User.query.filter_by(email = session['email']).first()
+    else:
+        user = None
     
     form = CommentForm()
     if form.validate_on_submit():
@@ -121,7 +175,7 @@ def index(room='000000'):
     
     comments = Comment.query.filter_by(room=room).order_by(db.desc(Comment.timestamp))
     ccount = comments.count()
-    return render_template('index.html', comments=comments, form=form, room=room, new=createRoom(), ccount=ccount)
+    return render_template('index.html', comments=comments, form=form, room=room, new=createRoom(), ccount=ccount, user=user)
 
 
 def static(path):
